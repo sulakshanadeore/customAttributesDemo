94d94e227536b2ab8064c01b21b36536b8438d08
deoresulakshana <deoresulakshana@gmail.com>
2023-10-23 12:08:33 +0530

    Add project files.

---
+++ customAttributesDemo/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customAttributesDemo
+{
+
+    [System.AttributeUsage(AttributeTargets.Class | AttributeTargets.Field| AttributeTargets.Interface, Inherited = true, AllowMultiple = true)]
+     class DeveloperAttribute : Attribute
+    {
+        // See the attribute guidelines at 
+        //  http://go.microsoft.com/fwlink/?LinkId=85236
+        readonly string positionalString;
+
+        // This is a positional argument
+        public DeveloperAttribute(string positionalString)
+        {
+            this.positionalString = positionalString;
+
+            // TODO: Implement code here
+
+            throw new NotImplementedException();
+        }
+
+        public string PositionalString
+        {
+            get { return positionalString; }
+        }
+
+        // This is a named argument
+        public int NamedInt { get; set; }
+
+        public int Devid { get; set; }
+        public string DevName { get; set; }
+    }
+
+
+    //public class DeveloperAttribute
+    //{
+
+    //}
+}
---
+++ customAttributesDemo/Products.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace customAttributesDemo
+{
+    [Developer("1", Devid = 102, DevName = "nikhil")]
+    [Developer("1",Devid =103,DevName ="Sunil")]
+    public class Books
+    {
+
+    }
+
+
+    [Developer("1", Devid = 102, DevName = "nikhil")]
+    public class Products
+    {
+        [Developer("1",Devid =101,DevName ="Arti")]
+        int _price;
+        [Developer("1", Devid = 101, DevName = "Arti")]
+        int _qty;
+
+        public int Prodid { get; set; }
+        public string Prodname { get; set; }
+    }
+}
